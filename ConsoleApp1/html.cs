f19ce6d27e8effda78942a0361fae460270078e0
2811733392@qq.com <2811733392@qq.com>
2021-04-23 10:37:16 +0800

    测试提交第五次

---
+++ ConsoleApp1/html.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int num = 55;
+			short sam = 15;
+			sam = 88;
+			sam = 99;
+
+
+			Console.WriteLine("Hello World!");
+			Console.Read();
+		}
+	}
+}

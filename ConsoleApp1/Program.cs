d4535a85b76bd47a34b19da8727914cdff6b8921
2811733392@qq.com <2811733392@qq.com>
2021-04-25 09:14:52 +0800

    循环作业前两题

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,31 +6,58 @@ namespace ConsoleApp1
 	{
 		static void Main(string[] args)
 		{
-			/*
-						作业：
+			/*作业：
 
-			将源栈同学姓名 / 昵称分别：
-			按进栈时间装入一维数组，
-			按座位装入二维数组，
-			并输出共有多少名同学。*/
-
-			string[] name = { "夏康平 \n昵称\n2020.2",
-				"陈国栋\n昵称\n2020.2",
-				"姜鹏\n昵称\n2020.2", 
-				"刘伟\n昵称\n2020.2", 
+			
+			/*分别用for循环和while循环输出：1,2,3,4,5 和 1,3,5,7,9*/
+			/*for (int i = 1; i < 6; i++)
+			{
+				Console.WriteLine(i);
+			}
+			int i = 1;
+			while (i<=9)
+			{
+				
+				Console.WriteLine(i);
+				i += 2;
+			}*/
+			/*用for循环输出存储在一维 / 二维数组里的源栈所有同学姓名 / 昵称
+			String[] name ={ "夏康平 \n昵称\n2020.2",
+/*				"陈国栋\n昵称\n2020.2",
+				"姜鹏\n昵称\n2020.2",
+				"刘伟\n昵称\n2020.2",
 				"胡涛\n昵称\n2020.2" };
 			
-			Console.WriteLine(name[0]);
-			string[,] seat = new string [2,3];
+						for (int i = 0; i < name.Length; i++)
+						{
+
+
+							Console.WriteLine(name[i]);
+
+						}*/
+
+
+			//二维数组
+/*			string[,] seat = new string[2, 3];
+
 			seat[0, 0] = name[0];
 			seat[0, 1] = name[1];
 			seat[1, 0] = name[2];
 			seat[1, 1] = name[3];
-			seat[1,2] = name[4];
-			Console.WriteLine(seat[1,2]);
-			//输出共有多少名学生
-			Console.WriteLine("共有"+name.Length+"名学生");
-		
+			seat[1, 2] = name[4];
+
+			for (int i = 0; i < 2; i++)
+			{
+				for (int j = 0; j < 3; j++)
+				{
+
+					Console.WriteLine(seat[i, j]);
+				}
+			}*/
+			
+
+
+
 		}
 	}
 }

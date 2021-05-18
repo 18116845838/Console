1ca70609ae783616d87744132c86ceaf2ddb79aa
2811733392@qq.com <2811733392@qq.com>
2021-05-18 16:33:47 +0800

    双向链表修改，未完成

--- ConsoleApp1/LinkNode/DLinkNode.cs
+++ ConsoleApp1/LinkNode/DLinkNode.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
 	public class DLinkNode
 	{
 		//	继续完成双向链表的测试和开发，实现：
-
+		
 		//   node3.InsertAfter(node1); 场景
 		//InerstBefore()：在某个节点前插入
 		//Delete()：删除某个节点
@@ -16,6 +16,7 @@ namespace ConsoleApp1
 
 		public DLinkNode Next { get; set; }
 		public DLinkNode Previous { get; set; }
+		private DLinkNode temp;
 		public void AddAfter(DLinkNode node)//在后面添加节点
 		{
 			if (this.Next != null && this.Previous != null)//插入节点
@@ -42,14 +43,14 @@ namespace ConsoleApp1
 
 		public void Delete(DLinkNode node)//删除节点
 		{
-			if (this.Previous != null && this.Next != null)//删除中间节点
+			if (node.Previous != null && node.Next != null)//删除中间节点
 			{
 				node.Next.Previous = node.Previous;
 				node.Previous.Next = node.Next;
 				node.Next = null;
 				node.Previous = null;
 			}
-			else if (this.Next == null)//删除最后一个节点的情况
+			else if (node.Next == null)//删除最后一个节点的情况
 			{
 				node.Previous.Next = null;
 				node.Previous = null;
@@ -62,6 +63,48 @@ namespace ConsoleApp1
 		}
 		public void Swap(DLinkNode node)//交换两个节点
 		{
+			if (this.Previous != null && node.Next != null)
+			{
+				node.Next.Previous = this;
+				temp = node.Next;
+				node.Next = this.Next;
+				this.Next.Previous = node;
+				this.Next = temp;
+				node.Previous.Next = this;
+				temp = node.Previous;
+				node.Previous = this.Previous;
+				this.Previous.Next = node;
+				this.Previous = temp;
+			}
+			else if (this.Previous == null)
+			{
+				node.Next.Previous = this;
+				temp = node.Next;
+				node.Next = this.Next;
+				this.Next.Previous = node;
+				this.Next = temp;
+				node.Previous.Next = this;
+				temp = node.Previous;
+				node.Previous = this.Previous;
+				this.Previous = temp;
+			}
+			else if (node.Next == null)
+			{
+				this.Next.Previous = node;
+				temp = node.Next;
+				node.Next = this.Next;
+				this.Previous.Next = node;
+				this.Next = temp;
+				node.Previous.Next = this;
+				temp = node.Previous;
+				node.Previous = this.Previous;
+				this.Previous = temp;
+			}
+			else if (this.Next==node)
+			{
+
+			}
+
 
 
 		}
--- TestProject1/DLinkNodeTest.cs
+++ TestProject1/DLinkNodeTest.cs
@@ -35,65 +35,87 @@ namespace TestProject1
 		}
 		[Test]//测试添加节点
 		public void AddAfterTest()
-		{
+		{   // 1 2 3 4
 			node3.AddAfter(node4);//在后面添加节点
 			Assert.AreEqual(node3, node4.Previous);
-			Assert.AreEqual(node4,node3.Next);
+			Assert.AreEqual(node4, node3.Next);
 			Assert.IsNull(node4.Next);
-
-			node2.AddAfter(node5);
-			Assert.AreEqual(node3, node5.Next);
-			Assert.AreEqual(node3, node5.Next);
+			// 1 2 3 4 5
+			node4.AddAfter(node5);
+			Assert.AreEqual(node3, node4.Previous);
+			Assert.AreEqual(node5, node4.Next);
+			Assert.AreEqual(node4, node5.Previous);
+			Assert.AreEqual(null, node5.Next);
 
 			//测试在前面添加节点
+			// 6 1 2 3 4 5
 			node1.AddBefor(node6);
+			Assert.IsNull(node6.Previous);
 			Assert.AreEqual(node6, node1.Previous);
 			Assert.AreEqual(node1, node6.Next);
-
+			Assert.AreEqual(node1, node2.Previous);
+			Assert.AreEqual(node2, node1.Next);
+			Assert.AreEqual(node2, node3.Previous);
 		}
 		[Test]//测试删除节点
 		public void DeleteTest()
 		{
-			node2.Delete(node2);
-			Assert.IsNull(node2.Previous);
-			Assert.AreEqual(node1.Next, node3);
-			Assert.AreEqual(node1,node3.Previous);
-			Assert.AreEqual(node3,node1.Next);
+			// [1] 2 3 
 			node1.Delete(node1);
-			Assert.AreEqual(node3.Previous, null);
-			Assert.AreEqual(node3.Next, null);
-			Assert.AreEqual(node1.Next, null);
-			Assert.AreEqual(null, node1.Previous);
-
+			Assert.IsNull(node1.Previous);
+			Assert.IsNull(node1.Next);
+			Assert.AreEqual(node2,node3.Previous);
+			Assert.AreEqual(node3, node2.Next);
 		}
 		[Test]
 		public void SwapTest()//测试交换节点
 		{
-			//Assert.AreEqual(node1.Previous, null);
-			//node2.Swap(node3);
-			//Assert.AreEqual(node2.Previous, node3);
-			//Assert.AreEqual(node2.Next, node4);
-			//Assert.AreEqual(node3.Next, node2);
-			//Assert.AreEqual(node1, node3.Previous);
-			//Assert.AreEqual(node1.Next,node3);
+			node3.AddAfter(node4);//先添加一个node4和node5
+			node4.AddAfter(node5);//       1 2 3 4 5
+								  //交换 --1 4 3 2 5
+			node2.Swap(node4);					 
+			Assert.AreEqual(null,node1.Previous);
+			Assert.AreEqual(node1,node4.Previous);
+			Assert.AreEqual(node4,node1.Next);
+			Assert.AreEqual(node4,node3.Previous);
+			Assert.AreEqual(node3,node4.Next);
+			Assert.AreEqual(node3,node2.Previous);
+			Assert.AreEqual(node2,node3.Next);
+			Assert.AreEqual(node2,node5.Previous);
+			Assert.AreEqual(node5, node2.Next);
+			Assert.AreEqual(null, node5.Next);
 
-			//1,[4],3,[2],5
-			//node2.Swap(node4);
-			//Assert.AreEqual(node1.Next, node4); 
-			//Assert.AreEqual(node1, node4.Previous);
+			//交换--头部为null值   2 4 3 1 5
+			node1.Swap(node2);
+			Assert.AreEqual(null,node2.Previous); 
+			Assert.AreEqual(node2,node4.Previous); 
+			Assert.AreEqual(node4,node2.Next); 
+			Assert.AreEqual(node4,node3.Previous); 
+			Assert.AreEqual(node3,node4.Next); 
+			Assert.AreEqual(node3,node1.Previous); 
+			Assert.AreEqual(node1,node3.Next); 
+			Assert.AreEqual(node1,node5.Previous); 
+			Assert.AreEqual(node5,node1.Next); 
+			Assert.AreEqual(null,node5.Next);
 
-			//Assert.AreEqual(node3.Next, node2);
-			//Assert.AreEqual(node2.Previous, node3);
-			//Assert.AreEqual(node2.Next, node5);
-			//Assert.AreEqual(node5.Previous,node2);
-			//Assert.AreEqual(node4, node3.Previous);
-
+			//交换--末尾为null值 2 5 3 1 4
+			node4.Swap(node5);
+			Assert.AreEqual(null, node2.Previous);
+			Assert.AreEqual(node2, node5.Previous);
+			Assert.AreEqual(node5, node2.Next);
+			Assert.AreEqual(node5, node3.Previous);
+			Assert.AreEqual(node3, node5.Next);
+			Assert.AreEqual(node3, node1.Previous);
+			Assert.AreEqual(node1, node3.Next);
+			Assert.AreEqual(node1, node4.Previous);
+			Assert.AreEqual(node4, node1.Next);
+			Assert.AreEqual(null, node4.Next);
 
 		}
 		[Test]
 		public void FindByTest()//根据节点查找某个节点
-		{ 
-		
+		{
+
 		}
 	}
 }
